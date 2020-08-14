9137c253aeea7c7b5ed6970794c299117095af1e
Peter Heldens <peter.heldens@live.nl>
2020-08-14 17:37:09 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace dotnet
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      for (int i = 0; i < 5; i++)
+      {
+        Console.WriteLine("Hello World!" + "-" + (i + 1) + " run");
+      }
+    }
+  }
+}
